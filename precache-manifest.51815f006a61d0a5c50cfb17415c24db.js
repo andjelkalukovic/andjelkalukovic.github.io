457042b1399b0901f6d5518aeacb80a4f574108f
Andjelka Lukovic <andjelkal@live.com>
2020-04-03 12:06:56 +0200

    io-test

---
+++ precache-manifest.51815f006a61d0a5c50cfb17415c24db.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a96e85aa5fdb92580612c3dffae6c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe82b1d9b71b536962c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c8eff69bdb2ae922c759",
+    "url": "/static/css/main.21461c3b.chunk.css"
+  },
+  {
+    "revision": "8fe82b1d9b71b536962c",
+    "url": "/static/js/2.f188ef0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f188ef0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8eff69bdb2ae922c759",
+    "url": "/static/js/main.6306a8aa.chunk.js"
+  },
+  {
+    "revision": "32adee4c6248cb45e4c5",
+    "url": "/static/js/runtime-main.fe592f61.js"
+  },
+  {
+    "revision": "358dae3360bf443b3a7eb8f9d194a07c",
+    "url": "/static/media/img1.358dae33.jpg"
+  },
+  {
+    "revision": "7b0e144cf6da7189809566327bede70e",
+    "url": "/static/media/img10.7b0e144c.jpg"
+  },
+  {
+    "revision": "17909ffda67cf86eccc831fe22a63e7a",
+    "url": "/static/media/img12.17909ffd.jpg"
+  },
+  {
+    "revision": "07a6d2be0e86c9a158de68538c55e54d",
+    "url": "/static/media/img14.07a6d2be.jpg"
+  },
+  {
+    "revision": "e31b888f7e4cee83a0ad686a5874f831",
+    "url": "/static/media/img1515.e31b888f.jpg"
+  },
+  {
+    "revision": "0f24aabe48e5dc62e5b3dfcfc96e02df",
+    "url": "/static/media/img3.0f24aabe.jpg"
+  },
+  {
+    "revision": "346ccbb4b0a42f255b04735246b81ccd",
+    "url": "/static/media/img4.346ccbb4.jpg"
+  },
+  {
+    "revision": "14c68e6299a22cfbe09ffcc74c2cfe35",
+    "url": "/static/media/img6.14c68e62.jpg"
+  },
+  {
+    "revision": "92d2e5acba08db9afe610ee80ee7d8e9",
+    "url": "/static/media/jpg7.92d2e5ac.jpg"
+  },
+  {
+    "revision": "8d4ea19ca6ac1075efe310b250a9380d",
+    "url": "/static/media/pngguruChef.com.8d4ea19c.png"
+  }
+]);
